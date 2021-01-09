bae98d4dd4155cf99d89fa12877c9430c86cd251
phx <945871257@qq.com>
2021-01-09 11:17:53 +0800

    create

---
+++ Assets/Scripts/WebAttr.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WebAttr : MonoBehaviour
+{
+    public float disapperTime;
+    public int damage;
+    private System.Random random = new System.Random();
+
+    void Start()
+    {
+        Destroy(gameObject, disapperTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Fish")
+        {
+            var rd = random.Next(0, 10);
+            if (rd > 7)
+            {
+                collision.SendMessage("TakeDamage", damage);
+            }
+        }
+    }
+}
